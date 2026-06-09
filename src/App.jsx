9278ed9048ff8013176ad32f2b4ce57dd78a211b
Balaji <balaji01122000@gmail.com>
2026-06-09 07:33:36 +0530

    new files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import tailwindcss from '@tailwindcss/vite'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+     <h1 className='bg-'>hello</h1>
+    </>
+  )
+}
+
+export default App
